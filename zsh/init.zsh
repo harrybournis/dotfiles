d2355f56c2e01d3a59815a738dcd78e56432bf5f
Harry Bournis <harrybournis@gmail.com>
2022-04-04 18:20:20 +0300

    Zsh: Disalbe lazy loading node and nvm

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -6,20 +6,20 @@ fi
 
 # Get rid of nvm lag
 # Source: https://medium.com/@dannysmith/little-thing-2-speeding-up-zsh-f1860390f92
-nvm() {
-    unset -f nvm
-    export [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  nvm "$@"
-}
+# nvm() {
+#     unset -f nvm
+#     export [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  nvm "$@"
+# }
 
-declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+# declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
 
-NODE_GLOBALS+=("node")
-NODE_GLOBALS+=("nvm")
+# NODE_GLOBALS+=("node")
+# NODE_GLOBALS+=("nvm")
 
-load_nvm () {
-    export NVM_DIR=~/.nvm
-    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-}
+# load_nvm () {
+#     export NVM_DIR=~/.nvm
+#     [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+# }
 
 # for cmd in "${NODE_GLOBALS[@]}"; do
 #     eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
