0e95c8696153e07dce4a8b340fa7f14f92ece157
Harry Bournis <harrybournis@gmail.com>
2023-04-27 22:34:53 +0300

    Emacs: demand evil-matchit, evil-lion disable debug-on-error

--- emacs.d.symlink/init.el
+++ emacs.d.symlink/init.el
@@ -134,7 +134,7 @@
                        "Clean up gc."
                        (setq gc-cons-threshold  100000000
                              gc-cons-percentage 0.1
-                             debug-on-error t)
+                             debug-on-error nil)
                        (garbage-collect)))
 
 ;;; init.el ends here
