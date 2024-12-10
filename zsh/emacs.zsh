4c86a323c248e254242db33e688a5d4ab2ea1800
Harry Bournis <harrybournis@gmail.com>
2024-12-10 21:35:02 +0200

    Zsh: Add .zshenv, autocd, organize emacs stuff

---
+++ zsh/emacs.zsh
@@ -0,0 +1,9 @@
+if [[ "$INSIDE_EMACS" ]]; then
+    PROMPT='%~$ '
+fi
+
+if [[ "$INSIDE_EMACS" = 'vterm' ]] \
+       && [[ -n ${EMACS_VTERM_PATH} ]] \
+       && [[ -f ${EMACS_VTERM_PATH}/etc/emacs-vterm-zsh.sh ]]; then
+    source ${EMACS_VTERM_PATH}/etc/emacs-vterm-zsh.sh
+fi
