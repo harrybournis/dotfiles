59daf0dabb366083f2006e67be3884e5d2ad2972
Harry Bournis <harrybournis@gmail.com>
2026-02-12 18:11:07 +0100

    Zsh: Init mise instead of asdf

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,7 +2,8 @@ export PATH="/usr/local/opt/rustup/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 
-# asdf
-export PATH="$HOME/.asdf/shims:$PATH"
-[[ -a $HOME/.asdf/plugins/java/set-java-home.zsh ]] &&
-    source $HOME/.asdf/plugins/java/set-java-home.zsh
+if command -v mise &> /dev/null; then
+    eval "$(mise activate zsh)"
+else
+    echo "Mise command not found. Make sure it is installed."
+fi
